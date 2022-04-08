a0735bcd7ac86c8d6181a8c90730e9737cc89358
Aprameyan-V <aprameyan.official@gmail.com>
2022-04-08 11:15:19 +0530

    empty commmit

--- Chat_bot/Functions.py
+++ Chat_bot/Functions.py
@@ -1,15 +1,37 @@
+from re import X
+from matplotlib.font_manager import json_load
 import pandas as pd
 import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+import json
 
 stemmer = PorterStemmer()
 
+with open ("contents.json","r") as f:
+    file = json.load(f) 
+
+
+
+
 def tokenize(sentence):
-    return(word_tokenize(sentence))
+    return word_tokenize(sentence)
+
 
 def stem(word):
-    return stemmer.stem(word,to_lowercase=True)
+    return stemmer.stem(word, to_lowercase=True)
 
-def bag_of_words(tokenized_sentence,all_words):
-    pass
+
+def bag_of_words(sentences):
+    vectorizer = CountVectorizer()
+    return vectorizer.fit_transform(sentences)
+
+
+
+patterns=[]
+for i in range(len(file["intents"])):
+    patterns.extend(file['intents'][i]['patterns'])
+
+X=bag_of_words(patterns)
+
